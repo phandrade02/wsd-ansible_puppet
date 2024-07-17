4f9e317abb5b56a27630fbc3bb969f25dd7c6aa1
phandrade02 <166563731+phandrade02@users.noreply.github.com>
2024-07-17 18:40:23 -0300

    Update deploy_ntpd.pp

--- deploy_ntpd.pp
+++ deploy_ntpd.pp
@@ -3,9 +3,16 @@ node 'app-vm1.fra1.internal', 'db-vm1.fra1.db', 'web-vm1.fra1.web' {
     ensure => installed,
   }
 
+  file { '/etc/ntp.conf':
+    ensure  => file,
+    content => template('ntp/ntp.conf.erb'),
+    require => Package['ntp'],
+    notify  => Service['ntp'],
+  }
+
   service { 'ntp':
     ensure    => running,
     enable    => true,
-    subscribe => Package['ntp'],
+    subscribe => File['/etc/ntp.conf'],
   }
 }
